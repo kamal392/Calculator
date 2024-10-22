e8707ad84a9ac85132f482b140a5c366f7de28df
kamal kishore <kamal.kishore981@gmail.com>
2024-10-22 11:19:45 -0400

    create components and modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
-
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 function App() {
   return (
-    <>
-      <h1>Calculator</h1>
-    </>
+    <div className={styles.calculator}>
+      <Display></Display>
+      <ButtonsContainer></ButtonsContainer>
+    </div>
   );
 }
 
---
+++ src/components/ButtonsContainer.jsx
@@ -0,0 +1,30 @@
+import styles from "./ButtonsContainer.module.css";
+const ButtonsContainer = () => {
+  const buttonsName = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div className={styles.buttonsContainer}>
+      {buttonsName.map((buttonName) => {
+        return <button className={styles.button}>{buttonName}</button>;
+      })}
+    </div>
+  );
+};
+export default ButtonsContainer;
